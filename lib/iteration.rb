c78b601c19cfe585436a59cdc273661cd3162218
Joshua Mclean <joshmcleana@gmail.com>
2020-02-29 20:12:51 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -45,7 +45,7 @@ r_i = 0
 while r_i < src.length do
 inner_array = src[r_i]
 if inner_array[0] && inner_array[1] % 2 == 0
-  total += inner_array
+  total += inner_array[0] + inner_array[1]
 end
 r_i += 1
 end
